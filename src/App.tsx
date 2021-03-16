283455012dbe2e0d13a0fdcdee5f481242777327
Igor Cossta <igorcossta21@gmail.com>
2021-03-16 12:17:06 -0300

    toggle theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import "react-calendar-heatmap/dist/styles.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Repo from "./pages/Repo";
 import Footer from "./components/Footer";
+import { ThemeName, themes } from "./styles/themes";
 
 function App() {
+  const [theme, setTheme] = useState<ThemeName>("light");
+  const current = themes[theme];
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Repo />} />
-      </Routes>
-      <GlobalStyles />
-      <Footer />
-    </BrowserRouter>
+    <ThemeProvider theme={current}>
+      <BrowserRouter>
+        <Header themeName={theme} setThemeName={setTheme} />
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:reponame" element={<Repo />} />
+        </Routes>
+        <GlobalStyles />
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
